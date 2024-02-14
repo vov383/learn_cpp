333a09ebddab92da7f30479e4d4f43624d82d67a
vov383 <dbtjr0682@gmail.com>
2024-02-14 14:32:30 +0900

    studied last days

---
+++ iostream_my_initials.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main()
+{
+    std::cout << "Y       Y      SSS  \n";
+    std::cout << "  Y   Y      S     S\n";
+    std::cout << "    Y        S      \n";
+    std::cout << "    Y          SSS  \n";
+    std::cout << "    Y              S\n";
+    std::cout << "    Y        S     S\n";
+    std::cout << "    Y          SSS  \n";
+    return 0;
+}
